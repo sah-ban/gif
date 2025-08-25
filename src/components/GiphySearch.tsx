67cb007dc3e1a2d8e0a2a6443af4199b20dfaa3e
sahban <sahbaan@proton.me>
2025-08-25 15:49:51 +0530

    add ability to quote cast

--- src/components/GiphySearch.tsx
+++ src/components/GiphySearch.tsx
@@ -12,6 +12,8 @@ import "react-farcaster-embed/dist/styles.css";
 export default function GiphySearch() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
   const [context, setContext] = useState<Context.MiniAppContext>();
+  const [showPopup, setShowPopup] = useState(false);
+  const [selected, setSelected] = useState<string | null>(null);
 
   useEffect(() => {
     const load = async () => {
@@ -42,10 +44,15 @@ export default function GiphySearch() {
   const castFid = searchParams.get("castFid");
 
   const casting = async (url: string) => {
-    if (castHash) {
+    if (castHash && selected === "Replying to") {
       sdk.actions.openUrl(
         `https://farcaster.xyz/~/compose?embeds[]=${url}&parentCastHash=${castHash}`
       );
+    } else if (castHash && selected === "Quote Casting") {
+      const hash = await quote(url, castHash.slice(0, 10));
+      if (hash) {
+        sdk.actions.close();
+      }
     } else {
       const hash = await cast(url);
       if (hash) {
@@ -56,15 +63,28 @@ export default function GiphySearch() {
   const cast = async (url: string): Promise<string | undefined> => {
     try {
       const result = await sdk.actions.composeCast({
-        embeds: [`${url}`],
+        embeds: [url],
       });
 
-      return result.cast?.hash;
-    } catch (error) {
-      console.error("Error composing cast:", error);
-      return undefined;
-    }
-  };
+    return result.cast?.hash;
+  } catch (error) {
+    console.error("Error composing cast:", error);
+    return undefined;
+  }
+};
+
+  const quote = async (url: string, hash: string): Promise<string | undefined> => {
+    try {
+      const result = await sdk.actions.composeCast({
+        embeds: [url, `https://farcaster.xyz/${profileData?.username}/${hash}`],
+      });
+
+    return result.cast?.hash;
+  } catch (error) {
+    console.error("Error composing cast:", error);
+    return undefined;
+  }
+};
 
   useEffect(() => {
     if (!context?.client.added) {
@@ -95,31 +115,35 @@ export default function GiphySearch() {
   useEffect(() => {
     if (castFid) {
       fetchProfile(castFid);
+      setShowPopup(true);
     }
   }, [context]);
 
-  if (!context)
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-900">
-        <div className="flex flex-col items-center justify-center text-white text-2xl p-4">
-          <p className="flex items-center justify-center text-center">
-            You need to access this mini app from inside a farcaster client
-          </p>
-          <div
-            className="flex items-center justify-center text-center bg-indigo-800 p-3 rounded-lg mt-4 cursor-pointer"
-            onClick={() => window.open("https://farcaster.xyz/cashlessman.eth")}
-          >
-            Open in Farcaster
-          </div>
-        </div>
-      </div>
-    );
+  // if (!context)
+  //   return (
+  //     <div className="flex items-center justify-center h-screen bg-gray-900">
+  //       <div className="flex flex-col items-center justify-center text-white text-2xl p-4">
+  //         <p className="flex items-center justify-center text-center">
+  //           You need to access this mini app from inside a farcaster client
+  //         </p>
+  //         <div
+  //           className="flex items-center justify-center text-center bg-indigo-800 p-3 rounded-lg mt-4 cursor-pointer"
+  //           onClick={() => window.open("https://farcaster.xyz/cashlessman.eth")}
+  //         >
+  //           Open in Farcaster
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 
   return (
     <div className="">
+      <QuoteOrReply />
       {castHash && profileData?.username && (
         <div className="mb-4">
-          <div className="text-white mb-4">Replying to @{profileData?.username}</div>
+          <div className="text-white mb-4">
+            {selected} @{profileData?.username}
+          </div>
           <div className="bg-[#16101e] text-white rounded-2xl shadow-lg max-w-xl w-full border border-[#2F3336]">
             <FarcasterEmbed username={profileData?.username} hash={castHash} />
           </div>
@@ -152,4 +176,52 @@ export default function GiphySearch() {
       </div>
     </div>
   );
+
+  function QuoteOrReply() {
+    const handleSelect = (choice: string) => {
+      setSelected(choice);
+      setShowPopup(false);
+    };
+    return (
+      <div className="text-white">
+        {showPopup && (
+          <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+            <div className="bg-white/10 border border-white/20 shadow-2xl rounded-2xl p-6 w-[90%] max-w-sm text-white relative backdrop-blur-xl">
+              <div className="flex gap-4 justify-center">
+                <button
+                  onClick={() => handleSelect("Quote Casting")}
+                  className="bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-600 transition flex items-center gap-2"
+                >
+                  <span className="text-4xl">&ldquo;</span>
+                  <span className="text-lg">Quote Cast</span>
+                </button>
+
+                <button
+                  onClick={() => handleSelect("Replying to")}
+                  className="bg-green-500 px-4 py-2 rounded-lg hover:bg-green-600 transition flex items-center gap-2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+
+                  <span className="text-lg">Reply</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
