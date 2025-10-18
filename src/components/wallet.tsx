78eb105ca0ee35c616e500a16331d925eb904221
sahban <sahbaan@proton.me>
2025-10-18 16:01:39 +0530

    update button state

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -238,7 +238,7 @@ const CheckInComponent: React.FC = () => {
 
           <button
             onClick={handleCheckIn}
-            disabled={isPending || isConfirming || checkedInToday}
+            disabled={isPending || isConfirming || checkedInToday || isConfirmed}
             className="px-2 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:bg-gray-400 disabled:cursor-not-allowed font-semibold"
           >
             {isPending
