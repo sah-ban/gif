e9423b8cbf87ef57e9a5d35b37748325801a20c6
sahban <sahbaan@proton.me>
2025-10-18 16:07:12 +0530

    update accountAssociation

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -5,9 +5,9 @@ export async function GET() {
     accountAssociation: {
       header:
         "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
-      payload: "eyJkb21haW4iOiJmYy1naWYudmVyY2VsLmFwcCJ9",
+      payload: "eyJkb21haW4iOiJnaWYuaXRzY2FzaGxlc3MuY29tIn0",
       signature:
-        "MHg3ODFmMjhhNDk2NDY2ZWQ3OTBlN2ZlMWE5ODYxMWQ2NTQ1MTk5ZWZiYzM0ZTk1MWM4MTVkM2FmZTAxZTFjYzkyNTQ5NjZhOGZiODY3NDQ1OGNlMDFiMDIzNGVjMWVhNzM2ODkwZjI4Zjc3ZmQzN2JkYmM0N2RmZDE3NzY3MGQ5NjFj",
+        "07HrfqIEkBXrpqGwIcPgDtH8rAO2b5sQpWf77Y6/tsEsJvKSFZtXzvA05hCIxnLnZCWBDg020WE0eiGTnJ8QuRs=",
     },
     frame: {
       version: "1",
