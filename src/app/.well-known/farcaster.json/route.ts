71ddb978bbbbdffb02c7937e0ac667d197d6a24d
sahban <sahbaan@proton.me>
2026-09-19 22:40:45 +0530

    fix: update account association logic based on request domain

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,14 +1,26 @@
-export async function GET() {
+export async function GET(request: Request) {
+  const domain = request.headers.get("host")?.split(":")[0] || "";
+
+  const oldDomain = "gif.itscashless.com";
+
+  const oldAccountAssociation = {
+    header:
+      "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
+    payload: "eyJkb21haW4iOiJnaWYuaXRzY2FzaGxlc3MuY29tIn0",
+    signature:
+      "07HrfqIEkBXrpqGwIcPgDtH8rAO2b5sQpWf77Y6/tsEsJvKSFZtXzvA05hCIxnLnZCWBDg020WE0eiGTnJ8QuRs=",
+  };
+
+  const newAccountAssociation = {
+    header: "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
+    payload: "eyJkb21haW4iOiJnaWYuc2FoYmFuLmRldiJ9",
+    signature: "zVji8d6bxsNGi8kld+ImO8B535SxvQq/CQi5q7MPPFxhEk7hB3f6fXBvTK4zeyrjuzmUk32UyYzQ/jXYILEb6hw="
+  };
+
   const appUrl = process.env.NEXT_PUBLIC_URL;
 
   const config = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
-      payload: "eyJkb21haW4iOiJnaWYuaXRzY2FzaGxlc3MuY29tIn0",
-      signature:
-        "07HrfqIEkBXrpqGwIcPgDtH8rAO2b5sQpWf77Y6/tsEsJvKSFZtXzvA05hCIxnLnZCWBDg020WE0eiGTnJ8QuRs=",
-    },
+    accountAssociation: domain === oldDomain ? oldAccountAssociation : newAccountAssociation,
     frame: {
       version: "1",
       name: "GIF",
